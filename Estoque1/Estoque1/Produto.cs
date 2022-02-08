3246ae569b797330c6a0760861878d2c7e0730fc
Leandro Machado <leoantunes.fm@gmail.com>
2022-02-07 22:25:59 -0300

    Criação da classe Produto e leitura dos dados -- Aula 42

---
+++ Estoque1/Estoque1/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estoque1
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public double VlTotalEstoque()
+        {
+            return Preco * Quantidade;
+        }
+    }
+}
---
+++ Estoque1/Estoque1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estoque1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do primeiro produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: ");
+
+        }
+    }
+}
