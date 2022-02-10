9d064456620d965ed12f320fa626b93b697802c2
Leandro Machado <leoantunes.fm@gmail.com>
2022-02-10 00:10:21 -0300

    Membros estáticos -- Aula 48

---
+++ Cambio/Cambio/ConversorMoeda.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cambio
+{
+    class ConversorMoeda
+    {
+        private static double IOF = 6.0;
+
+        private static double CalculaIOF(double valorCompra, double cotacao)
+        {
+            return (valorCompra * (IOF / 100.0)) * cotacao;
+        }
+
+        public static double ValorAPagar(double valorCompra, double cotacao)
+        {
+            return (valorCompra * cotacao) + CalculaIOF(valorCompra, cotacao);
+        }
+    }
+}
---
+++ Cambio/Cambio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cambio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double valorCompra = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valorAPagar = ConversorMoeda.ValorAPagar(valorCompra, cotacao);
+
+            Console.WriteLine("Valor a ser pago em reais = " + valorAPagar.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
