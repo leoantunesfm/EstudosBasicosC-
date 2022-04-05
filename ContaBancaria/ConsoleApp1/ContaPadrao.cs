0fe782d7d1b5e986250eb8af287295343cd58f45
Leandro Machado <leandro.machado@ibid.com.br>
2022-04-04 22:39:45 -0300

    Implementação completa do exercício

---
+++ ContaBancaria/ConsoleApp1/ContaPadrao.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class ContaPadrao
+    {
+        public int NrConta { get; set; }
+        public string NmTitular { get; private set; }
+        public double Saldo { get; private set; }
+
+        public ContaPadrao(int nrConta, string nmTitular)
+        {
+            NrConta = nrConta;
+            NmTitular = nmTitular;
+        }
+
+        public ContaPadrao (int nrConta, string nmTitular, double saldo) : this (nrConta, nmTitular)
+        {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        } 
+
+        public void Saque(double valor)
+        {
+            Saldo -= (valor + 5.0);
+        }
+
+        public override string ToString()
+        {
+            return
+                "Conta: "
+                + NrConta
+                + ", Titular: "
+                + NmTitular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ ContaBancaria/ConsoleApp1/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+namespace ContaBancaria
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o número da conta: ");
+            int nrConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o nome do titular: ");
+            string nmTitular = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char opcao = char.Parse(Console.ReadLine());
+
+            double saldo = 0.0;
+
+            if (opcao.Equals('s') || opcao.Equals('S'))
+            {
+                Console.WriteLine();
+                Console.Write("Entre o valor de depósito inicial: ");
+                saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            ContaPadrao conta = new ContaPadrao(nrConta, nmTitular, saldo);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com o valor do depósito: ");
+            double valor = double.Parse(Console.ReadLine());
+            conta.Deposito(valor);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta bancária atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com o valor do saque: ");
+            valor = double.Parse(Console.ReadLine());
+            conta.Saque(valor);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta bancária atualizados:");
+            Console.WriteLine(conta);
+
+            Console.ReadLine();
+        }
+    }
+}
