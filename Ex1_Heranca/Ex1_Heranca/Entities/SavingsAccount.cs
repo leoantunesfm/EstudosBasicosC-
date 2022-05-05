9e6db057bc1496a8dfba3a020626ec7869c85460
Leandro Machado <leoantunes.fm@gmail.com>
2022-05-04 21:36:54 -0300

    Implementação de sobreposição

--- Ex1_Heranca/Ex1_Heranca/Entities/SavingsAccount.cs
+++ Ex1_Heranca/Ex1_Heranca/Entities/SavingsAccount.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace Ex1_Heranca.Entities
 {
-    class SavingsAccount
+    class SavingsAccount : Account
     {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance(double amount)
+        {
+            Balance += Balance * (InterestRate / 100.0);
+        }
+
+        public override void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
     }
 }
