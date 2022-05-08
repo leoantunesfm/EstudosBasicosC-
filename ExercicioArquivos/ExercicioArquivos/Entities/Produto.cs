6b8a49d408fa6696f62257f725b46fdd3fca2b22
Leandro Machado <leoantunes.fm@gmail.com>
2022-05-08 00:18:13 -0300

    Exercicio manipulacao de arquivos

---
+++ ExercicioArquivos/ExercicioArquivos/Entities/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioArquivos.Entities
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto() { }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double VendaTotal()
+        {
+            return Preco * Quantidade;
+        }
+    }
+}
---
+++ ExercicioArquivos/ExercicioArquivos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using ExercicioArquivos.Entities;
+using System.Globalization;
+
+namespace ExercicioArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = @"C:\temp\VendasAnalitico.csv";
+                List<Produto> produtos = new List<Produto>();
+
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string nome = line[0];
+                        double preco = double.Parse(line[1], CultureInfo.InvariantCulture);
+                        int quantidade = int.Parse(line[2]);
+
+                        produtos.Add(new Produto(nome, preco, quantidade));
+                    }
+                }
+
+                string targetFolderPath = Path.GetDirectoryName(path) + @"\out";
+                Directory.CreateDirectory(targetFolderPath);
+                string targetPath = targetFolderPath + @"\VendasSintetico.csv";
+
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (Produto produto in produtos)
+                    {
+                        sw.WriteLine(produto.Nome + ", $ " + produto.VendaTotal().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Erro: " + ex.Message);
+            }
+        }
+    }
+}
