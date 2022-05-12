177671e32743099dbc8eae63cf93650df85d4ea2
Leandro Machado <leandro.machado@ibid.com.br>
2022-05-12 16:47:57 -0300

    Exercício exemplo Dictionary

---
+++ Exemplo_Dictionary/Exemplo_Dictionary/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Exemplo_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Dictionary<string, int> registrosVotacao = new Dictionary<string, int>();
+                Console.Write("Entre com o caminho do arquivo: ");
+                string path = Console.ReadLine();
+
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string candidato = line[0];
+                        int votos = int.Parse(line[1]);
+
+                        if (registrosVotacao.ContainsKey(candidato))
+                        {
+                            registrosVotacao[candidato] += votos;
+                        }
+                        else
+                        {
+                            registrosVotacao.Add(candidato, votos);
+                        }
+                    }
+                }
+
+                Console.WriteLine();
+                foreach (var item in registrosVotacao)
+                {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
