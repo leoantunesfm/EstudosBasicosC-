99debd91cb09b02ccd8a462c4f7b300a21dc117f
Leandro Machado <leoantunes.fm@gmail.com>
2022-05-21 20:27:45 -0300

    Exercício Expressão Lambda

---
+++ Exemplo_lambda/Exemplo_lambda/Entidades/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exemplo_lambda.Entidades
+{
+    class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario() { }
+
+        public Funcionario(string nome, string email, double salario)
+        {
+            Nome = nome;
+            Email = email;
+            Salario = salario;
+        }
+    }
+}
---
+++ Exemplo_lambda/Exemplo_lambda/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using Exemplo_lambda.Entidades;
+
+namespace Exemplo_lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o caminho completo do arquivo: ");
+            string path = Console.ReadLine();
+
+            List<Funcionario> funcionarios = new List<Funcionario>();
+
+            using(StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] dados = sr.ReadLine().Split(',');
+                    string nome = dados[0];
+                    string email = dados[1];
+                    double salario = double.Parse(dados[2], CultureInfo.InvariantCulture);
+
+                    funcionarios.Add(new Funcionario(nome, email, salario));
+                }
+            }
+
+            Console.Write("Entre com o salário: ");
+            double salarioInformado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("E-mails das pessoas com salário maior que " + salarioInformado.ToString("F2", CultureInfo.InvariantCulture) + ": ");
+
+            var emails = funcionarios.Where(f => f.Salario > salarioInformado).OrderBy(f => f.Email).Select(f => f.Email);
+
+            foreach(string email in emails)
+            {
+                Console.WriteLine(email);
+            }
+
+            var soma = funcionarios.Where(f => f.Nome[0] == 'M').Select(f => f.Salario).Sum();
+
+            Console.Write("Soma dos salario das pessoas com nome iniciado em M: " + soma.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
